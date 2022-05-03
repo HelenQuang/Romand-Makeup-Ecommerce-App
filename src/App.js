ec9a93c5cd7b9b59567bbf8fb6d5578921da2b22
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-05-03 22:56:54 +0300

    Add pages, components, css files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import "./App.css";
+import "../src/UI/general.css";
+import "../src/UI/style.css";
+import HomePage from "../src/pages/HomePage";
+import LogInPage from "../src/pages/LogInPage";
+import SignUpPage from "../src/pages/SignUpPage";
+import CartPage from "../src/pages/CartPage";
+import CheckoutPage from "../src/pages/CheckoutPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HomePage />
     </div>
   );
 }
