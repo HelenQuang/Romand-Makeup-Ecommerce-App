68ce10482657c3ff2790e980582175ee3e963198
Helen Quang <94285120+HelenQuang@users.noreply.github.com>
2022-05-04 21:39:32 +0300

    Complete navbar and promotionbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,29 +4,35 @@ import AccountBtn from "./AccountBtn";
 
 const NavBar = () => {
   return (
-    <header className="header">
-      <nav className="main-nav">
-        <ul className="main-nav-list">
-          <a href="/">
-            <img className="logo" alt="Romand logo" src={logo} />
-          </a>
-          <li>
-            <a className="main-nav-link" href="/lip">
-              Our Lip Products
+    <>
+      <header className="header">
+        <nav className="main-nav">
+          <ul className="main-nav-list">
+            <a href="/">
+              <img className="logo" alt="Romand logo" src={logo} />
             </a>
-          </li>
-          <li>
-            <a className="main-nav-link" href="/eye">
-              Our Eye Products
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <div className="nav-btn">
-        <CartBtn />
-        <AccountBtn />
+            <li>
+              <a className="main-nav-link" href="/lip">
+                Our Lip Products
+              </a>
+            </li>
+            <li>
+              <a className="main-nav-link" href="/eye">
+                Our Eye Products
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <div className="nav-btn">
+          <CartBtn />
+          <AccountBtn />
+        </div>
+      </header>
+      <div className="promotion-bar">
+        <span></span>
       </div>
-    </header>
+    </>
   );
 };
 
